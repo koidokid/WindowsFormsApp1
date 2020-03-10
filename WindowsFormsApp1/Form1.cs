0c22536fe695494688b3864c432b58b7a93c1ccb
koidokid <iskoidokid@gmail.com>
2020-03-10 15:27:00 +0900

    First Commit

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			ListViewItem listViewItem = new ListViewItem();
+			listViewItem.Text = textBox1.Text;
+			listViewItem.SubItems.Add(textBox2.Text);
+			listViewItem.SubItems.Add(textBox3.Text);
+			listViewItem.SubItems.Add(textBox4.Text);
+			this.listView1.Items.Add(listViewItem);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			foreach (ListViewItem item in this.listView1.SelectedItems)
+			{
+				this.listView1.Items.Remove(item);
+			}
+		}
+	}
+}
